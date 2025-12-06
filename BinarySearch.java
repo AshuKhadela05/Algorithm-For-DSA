cc8a9aef0b71d01a60d10fbae125826c9ad26123
Ashish Khadela <jupg24msc20378@jainuniversity.ac.in>
2025-12-06 14:48:51 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -28,12 +28,12 @@ public class BinarySearch {
         
         int low = 0;                 // Starting index of the search range   
         int high = arr.length - 1;   // Ending index of the search range     
-        
+
         // Continue searching while the range is valid
         while (low <= high) {
             // int mid = (low + high) / 2;  // Find the middle index
             int mid = low + (high - low) / 2;
-
+            
             // If the middle element is the target, return its index
             if (arr[mid] == target) {
                 return mid;
@@ -56,6 +56,7 @@ public class BinarySearch {
 // OUTPUT:
 // Sorted List:
 // Element 11 found at index 0
+
 
 
 
